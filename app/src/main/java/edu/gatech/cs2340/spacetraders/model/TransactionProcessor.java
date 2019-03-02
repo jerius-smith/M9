9ad9481936786675d7b5e79f999f2ba163fc05cc
Shrey Amin <samin36@gatech.edu>
2019-03-01 20:55:16 -0500

    Universe Stuff Added

--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/TransactionProcessor.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/TransactionProcessor.java
@@ -2,6 +2,25 @@ package edu.gatech.cs2340.spacetraders.model;
 
 public class TransactionProcessor {
 
+    // You cannot buy more goods than the cargo capacity
+    public static boolean validateCargoCapacity(Player player, Ship toValidate) {
+        return (player.getInventory().getTotalStock() < toValidate.getCargoCapacity());
+    }
+
+    // You cannot buy more goods than you have money
+    public static boolean validatePlayerMoney(Player player, double marketPrice) {
+        return (player.getCredits() >= marketPrice);
+    }
+
+    // You cannot sell more goods than you own
+    public static boolean validateSellingGoods(Player player) {
+        return player.getInventory().getTotalStock() > 0;
+    }
+
+    public boolean buyItem(Player player, Good toBuy) {
+
+    }
+
 
 
 
